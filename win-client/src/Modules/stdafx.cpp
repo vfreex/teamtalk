06751c9eb544dbbd0da498ef146d5048068fec61
Yuxiang Zhu <vfreex@gmail.com>
2019-06-18 02:44:37 +0800

    fix Windows client build

--- win-client/src/Modules/stdafx.cpp
+++ win-client/src/Modules/stdafx.cpp
@@ -4,4 +4,6 @@
 
 #include "stdafx.h"
 
-
+// Workaround error LNK2005: _DllMain@12 already defined in MSVCRT.lib(dllmain.obj)
+// https://social.msdn.microsoft.com/Forums/en-US/0d78aa6b-1e87-4c01-a4a7-691335b7351a/how-to-build-mfc-application-dll-in-visual-c-2010
+extern "C" { int _afxForceUSRDLL; }
